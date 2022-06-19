6f3565b5c0652fc29eebd347c4831e7d3ef3b904
Onyiana <adaurecananaba@gmail.com>
2022-06-19 12:35:26 +0100

    project re done

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
@@ -37,4 +38,5 @@ typedef struct flags_printf
 char *c;
 int (*f)(va_list);
 } flags_p;
+
 #endif
